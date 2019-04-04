d65fb108d5fbafe48b86b1612876cd44865e2ded
aras armani <aras.armani2014@gmail.com>
2019-04-04 13:17:58 +0300

    categories component is create

---
+++ src/container/categories/categories.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import './style.css';
+
+
+class CategoriesComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <p>welcome to categories.</p>
+            </div>
+         );
+    }
+}
+ 
+export default CategoriesComponent;
