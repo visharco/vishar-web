d75b2c21804ae959bb854b937e8218d6d430ec55
aras armani <aras.armani2014@gmail.com>
2019-04-04 10:48:53 +0300

    footer styleing is fixed

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -14,6 +14,23 @@ class FooterComponent extends Component {
                 <div className="container">
                     <div className="footer-split ">
                         <h1>عضویت در خبرنامه</h1>
+                        <div className="footer-newslater">
+                            <div className="newslater-container">
+                                 <div className="footer-newslater-input-container">
+
+                                 </div>
+                                 <div className="footer-newslater-button-container">
+                                    <p>عضویت</p>
+                                 </div>
+                            </div>
+                            <div className="footer-social-network">
+                                <div className="footer-social"></div>
+                                <div className="footer-social"></div>
+                                <div className="footer-social"></div>
+                                <div className="footer-social"></div>
+                            </div>
+
+                        </div>
                     </div>
 
                     <div className="footer-split ">
