c09b8a46afc54f41f88b6f4a03eabb79dcd8483e
aras armani <aras.armani2014@gmail.com>
2019-04-04 10:24:52 +0300

    footer menu is fixed

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -11,7 +11,29 @@ class FooterComponent extends Component {
     render() { 
         return ( 
             <div className="footer">
-                <p>footer is ther</p>
+                <div className="container">
+                    <div className="footer-split ">
+                        <h1>عضویت در خبرنامه</h1>
+                    </div>
+
+                    <div className="footer-split ">
+                        <h1>منوها</h1>
+                        <div className="footer-menu">
+                            <ul>
+                                <li>درباره ما</li>
+                                <li>بلاگ</li>
+                                <li>تماس باما</li>
+                            </ul>
+                            <ul>
+                                <li>گتگوری ها</li>
+                                <li>طراحان</li>
+                                <li>پروژه ها</li>
+                            </ul>
+                 
+                        </div>
+                    </div>
+
+                </div>
             </div>
          );
     }
