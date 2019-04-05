7c4b49188ae8fa5931ae59a6133d0c7977bc7136
moji0098 <darzimojtaba@gmail.com>
2019-04-06 01:06:19 +0430

    about us fixed

--- src/container/aboutUs/aboutUs.js
+++ src/container/aboutUs/aboutUs.js
@@ -1,26 +1,26 @@
-import React, {Component} from 'react';
-import './style.css';
+import React, { Component } from 'react';
+
+
+
+import AboutUsPart1 from '../../component/aboutUs/aboutUsPart1/aboutUsPart1';
+import AboutUsPart2 from '../../component/aboutUs/aboutUsPart2/aboutUsPart2';
 
 
 
 class AboutUsComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
-            <div className="container-fluid">
-                <p>welcome to About us page </p>
-                <p>welcome to About us page </p>
-                <p>welcome to About us page </p>
-                <p>welcome to About us page </p>
-                <p>welcome to About us page </p>
-                <p>welcome to About us page </p>
-                <p>welcome to About us page </p>
+    render() {
+        return (
+            <div>
+                <AboutUsPart1 />
+                <AboutUsPart2 />
+                
             </div>
-         );
+        );
     }
 }
- 
+
 export default AboutUsComponent;
