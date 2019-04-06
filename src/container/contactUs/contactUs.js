702691ca2adcfe03ed3ddd29f1bc3b4697b59eb8
moji0098 <darzimojtaba@gmail.com>
2019-04-06 10:43:01 +0430

    2

---
+++ src/container/contactUs/contactUs.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './style.css';
+
+
+
+class contactUs extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div className="contactUs">
+                <div className="container-fluid">
+                    <div className="container">
+                        <p>welcome to About us page </p>
+                        <p>welcome to About us page </p>
+                        <p>welcome to About us page </p>
+                        <p>welcome to About us page </p>
+                        <p>welcome to About us page </p>
+                        <p>welcome to About us page </p>
+                        <p>welcome to About us page </p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default contactUs;
