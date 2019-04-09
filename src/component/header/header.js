0aa79ef6009cc7510128813dbfed67714baa8bba
moji0098 <darzimojtaba@gmail.com>
2019-04-09 19:16:13 +0430

    index res part1

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Link } from 'react-router';
+import { Link } from 'react-router';
 
 //
 //
@@ -11,28 +11,74 @@ import './style.css';
 class HeaderComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            openHumberger: false
+        }
     }
+
+
+    menuTablet = React.createRef()
+
+    openHumberger = (e) => {
+        this.setState((prevState) => {
+            return {
+                openHumberger: !prevState.openHumberger
+            }
+        })
+        if (!this.state.openHumberger) {
+            this.menuTablet.current.className = 'headerMenuDown'
+        } else {
+            this.menuTablet.current.className = 'headerMenuUp'
+
+        }
+    }
+
+
+
     render() {
+
+        let line1 = ['line line1']
+        let line2 = ['line line2']
+        let line3 = ['line line3']
+        if (this.state.openHumberger) {
+            line1 = ['lineX line1move']
+            line2 = ['lineX line2move']
+            line3 = ['lineX line3move']
+        } else {
+            line1 = ['line line1']
+            line2 = ['line line2']
+            line3 = ['line line3']
+        }
+
         return (
             <div className="header">
                 <div className="container-fluid" >
                     <div className="container" >
-                        <div className="header-menu" >
-                            <div className="menu-login" >
-                                <p className="menu-login-text">ثبت نام / ورود</p>
+
+                        <div className="menuTablet" >
+                            <div className="headerMenuUp" ref={this.menuTablet}  >
+                                <div className="menu-login" onClick={this.openHumberger}>
+                                    <p className="menu-login-text">ثبت نام / ورود</p>
+                                </div>
+                                <ul className="menu-links">
+                                    <Link onClick={this.openHumberger} to="/contactUs" ><li className="menu-link">تماس ما</li></Link>
+                                    <Link onClick={this.openHumberger} to="/blog" ><li className="menu-link">بلاگ</li></Link>
+                                    <Link onClick={this.openHumberger} to="/aboutUs" ><li className="menu-link">درباره ما</li></Link>
+                                    <Link onClick={this.openHumberger} to="/projects" ><li className="menu-link">پروژه ها</li></Link>
+                                    <Link onClick={this.openHumberger} to="/designers" ><li className="menu-link">طراحان</li></Link>
+                                    <Link onClick={this.openHumberger} to="/categories" ><li className="menu-link">دسته بندی</li></Link>
+                                </ul>
                             </div>
-                            <ul className="menu-links">
-                                <Link to="/contactUs" ><li className="menu-link">تماس ما</li></Link>
-                                <Link to="/blog" ><li className="menu-link">بلاگ</li></Link>
-                                <Link to="/aboutUs" ><li className="menu-link">درباره ما</li></Link>
-                                <Link to="/projects" ><li className="menu-link">پروژه ها</li></Link>
-                                <Link to="/designers" ><li className="menu-link">طراحان</li></Link>
-                                <Link to="/categories" ><li className="menu-link">دسته بندی</li></Link>
-                            </ul>
-                        </div>
-                        <div className="header-logo" >
-                        <Link to="/" ><img src={logo} alt="لوگو" /></Link>
+                            
+
+                            <div className="hamburger" onClick={(e) => this.openHumberger(e)} ref={this.humberger}>
+                                <span className={line1.join(' ')}></span>
+                                <span className={line2.join(' ')} ></span>
+                                <span className={line3.join(' ')} ></span>
+                            </div>
+                            <div className="header-logo" >
+                                <Link to="/" ><img src={logo} alt="لوگو" /></Link>
+                            </div>
                         </div>
                     </div>
                 </div>
