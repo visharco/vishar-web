48c42f0ad5af51617e14661e6b1663db1336da40
aras armani <aras.armani2014@gmail.com>
2019-04-16 17:54:54 +0300

    sdsd

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -65,7 +65,7 @@ class HeaderComponent extends Component {
                                 </a>
                                 <ul className="menu-links">
                                     <Link onClick={this.openHumberger} to="/contactUs" ><li className="menu-link">تماس ما</li></Link>
-                                    <Link onClick={this.openHumberger} to="/blog" ><li className="menu-link">بلاگ</li></Link>
+                                    <a href="http://www.blog.vishar.com"  ><li className="menu-link">بلاگ</li></a>
                                     <Link onClick={this.openHumberger} to="/aboutUs" ><li className="menu-link">درباره ما</li></Link>
                                     <Link onClick={this.openHumberger} to="/projects" ><li className="menu-link">پروژه ها</li></Link>
                                     <Link onClick={this.openHumberger} to="/designers" ><li className="menu-link">طراحان</li></Link>
