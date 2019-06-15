7404cfa76773d27800ed4150bc02ca4d7e72535a
aras armani <aras.armani2014@gmail.com>
2019-06-15 21:02:56 +0300

    fixed all

--- src/component/index/getStarted/getStarted.js
+++ src/component/index/getStarted/getStarted.js
@@ -23,12 +23,11 @@ class GetStarted extends Component {
                     <div className="container">
                         <div className="get-title">
                             <div className="get-title-box"  data-aos="fade-left"  >
-                                <div className="get-title-up "  >
-                                    <h1 >ویشــــــار </h1>
-                                    <h1> طراحان بیدار</h1>
+                                <div className="get-title-up "  > 
+                                    <h2> ویشار، طراحان همیشه بیدار</h2>
                                 </div>
                                 <h2 dir="rtl" className="get-title-down" >
-                                طراحان ویشار بر آن است با توجه به درک و شناخت نیاز سفارش دهنده، بهترین و متنوع ترین طرح ها را در سریعترین زمان ممکن توسط طراحان مجرب کشور بصورت مستقیم به کارفرما ارائه دهد.
+                                ویشار بستریست که طراحان مجرب سراسر کشور را به مشتریان و کارفرمایان متصل می کند. درک دقیق نیاز سفارش دهنده، کیفیت، تنوع طرح ها و سرعت تحویل پروژه از ویژگی های بارز ویشار می باشد.
                                 </h2>
                                 <div to="/" className="get-button-box" onClick={this.goto} >
                                     <p className="get-button" data-aos="flip-right">ایجاد حساب کاربری</p>
